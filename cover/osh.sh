d4dc8604cf14ffa86d18aeb09f7f51381c2812ef
Evan Foss <evanfoss@gmail.com>
2013-12-24 17:08:21 -0500

    working on a cover/front panel

---
+++ cover/osh.sh
@@ -0,0 +1,13 @@
+rm test.fab.gbr
+mv test.bottom.gbr test.gbl
+mv test.bottommask.gbr test.gbs
+mv test.bottomsilk.gbr test.gbo
+mv test.group1.gbr test.g2l
+mv test.group2.gbr test.g3l
+mv test.outline.gbr test.gko
+mv test.plated-drill.cnc test.xln
+mv test.top.gbr test.gtl
+mv test.topmask.gbr test.gts
+mv test.topsilk.gbr test.gto
+
+
