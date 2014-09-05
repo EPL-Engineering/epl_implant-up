b2316c99736954a10ae37dc34ec10c3737bce27b
Evan Foss <evanfoss@gmail.com>
2014-09-05 14:53:49 -0500

    cleanup removal of adc

---
+++ headboard-i2s-art.cmd
@@ -0,0 +1,99 @@
+# Pin name action command file
+
+# Start of element TP1106
+ChangePinName(TP1106, 1, 1)
+
+# Start of element TP1105
+ChangePinName(TP1105, 1, 1)
+
+# Start of element TP1104
+ChangePinName(TP1104, 1, 1)
+
+# Start of element TP1103
+ChangePinName(TP1103, 1, 1)
+
+# Start of element CONN1103
+ChangePinName(CONN1103, 7, 7)
+ChangePinName(CONN1103, 5, 5)
+ChangePinName(CONN1103, 3, 3)
+ChangePinName(CONN1103, 1, 1)
+ChangePinName(CONN1103, 8, 8)
+ChangePinName(CONN1103, 6, 6)
+ChangePinName(CONN1103, 4, 4)
+ChangePinName(CONN1103, 2, 2)
+
+# Start of element TP1102
+ChangePinName(TP1102, 1, 1)
+
+# Start of element R1104
+ChangePinName(R1104, 1, 1)
+ChangePinName(R1104, 2, 2)
+
+# Start of element R1101
+ChangePinName(R1101, 1, 1)
+ChangePinName(R1101, 2, 2)
+
+# Start of element R1102
+ChangePinName(R1102, 1, 1)
+ChangePinName(R1102, 2, 2)
+
+# Start of element R1103
+ChangePinName(R1103, 1, 1)
+ChangePinName(R1103, 2, 2)
+
+# Start of element TP1101
+ChangePinName(TP1101, 1, 1)
+
+# Start of element D1101
+ChangePinName(D1101, 4, Vss)
+ChangePinName(D1101, 3, IN2)
+ChangePinName(D1101, 2, IN1)
+ChangePinName(D1101, 1, GND)
+
+# Start of element D1100
+ChangePinName(D1100, 4, Vss)
+ChangePinName(D1100, 3, IN2)
+ChangePinName(D1100, 2, IN1)
+ChangePinName(D1100, 1, GND)
+
+# Start of element R1100
+ChangePinName(R1100, 1, 1)
+ChangePinName(R1100, 2, 2)
+
+# Start of element C1104
+ChangePinName(C1104, 2, 2)
+ChangePinName(C1104, 1, 1)
+
+# Start of element C1103
+ChangePinName(C1103, 2, 2)
+ChangePinName(C1103, 1, 1)
+
+# Start of element MIC1101
+ChangePinName(MIC1101, 9, GND)
+ChangePinName(MIC1101, 8, CHIPEN)
+ChangePinName(MIC1101, 7, Vdd)
+ChangePinName(MIC1101, 6, GND)
+ChangePinName(MIC1101, 5, GND)
+ChangePinName(MIC1101, 4, L/R)
+ChangePinName(MIC1101, 3, WS)
+ChangePinName(MIC1101, 2, SD)
+ChangePinName(MIC1101, 1, SCK)
+
+# Start of element C1102
+ChangePinName(C1102, 2, 2)
+ChangePinName(C1102, 1, 1)
+
+# Start of element C1101
+ChangePinName(C1101, 2, 2)
+ChangePinName(C1101, 1, 1)
+
+# Start of element MIC1100
+ChangePinName(MIC1100, 9, GND)
+ChangePinName(MIC1100, 8, CHIPEN)
+ChangePinName(MIC1100, 7, Vdd)
+ChangePinName(MIC1100, 6, GND)
+ChangePinName(MIC1100, 5, GND)
+ChangePinName(MIC1100, 4, L/R)
+ChangePinName(MIC1100, 3, WS)
+ChangePinName(MIC1100, 2, SD)
+ChangePinName(MIC1100, 1, SCK)
