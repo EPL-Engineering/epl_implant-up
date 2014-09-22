7d9b3ca08b069fed626e5dffca5fe351024e1f0b
Evan Foss <evanfoss@gmail.com>
2014-09-22 17:29:12 -0500

    trying to finish spi bus adapter for utd boards

--- spi2flatflex-art.cmd
+++ spi2flatflex-art.cmd
@@ -1,23 +1,48 @@
 # Pin name action command file
 
-# Start of element CONN703
-ChangePinName(CONN703, 20, 20)
-ChangePinName(CONN703, 19, 19)
-ChangePinName(CONN703, 18, 18)
-ChangePinName(CONN703, 17, 17)
-ChangePinName(CONN703, 16, 16)
-ChangePinName(CONN703, 15, 15)
-ChangePinName(CONN703, 14, 14)
-ChangePinName(CONN703, 13, 13)
-ChangePinName(CONN703, 12, 12)
-ChangePinName(CONN703, 11, 11)
-ChangePinName(CONN703, 10, 10)
-ChangePinName(CONN703, 9, 9)
-ChangePinName(CONN703, 8, 8)
-ChangePinName(CONN703, 7, 7)
-ChangePinName(CONN703, 6, 6)
-ChangePinName(CONN703, 5, 5)
-ChangePinName(CONN703, 4, 4)
-ChangePinName(CONN703, 3, 3)
-ChangePinName(CONN703, 2, 2)
-ChangePinName(CONN703, 1, 1)
+# Start of element CONN99
+ChangePinName(CONN99, 5, 5)
+ChangePinName(CONN99, 3, 3)
+ChangePinName(CONN99, 1, 1)
+ChangePinName(CONN99, 4, 4)
+ChangePinName(CONN99, 2, 2)
+
+# Start of element TP_3V3
+ChangePinName(TP_3V3, 1, 1)
+
+# Start of element TP_GND
+ChangePinName(TP_GND, 1, 1)
+
+# Start of element TP_SCK
+ChangePinName(TP_SCK, 1, 1)
+
+# Start of element TP_MISO
+ChangePinName(TP_MISO, 1, 1)
+
+# Start of element TP_MOSI
+ChangePinName(TP_MOSI, 1, 1)
+
+# Start of element TP_SS
+ChangePinName(TP_SS, 1, 1)
+
+# Start of element CONNJ3
+ChangePinName(CONNJ3, 20, 20)
+ChangePinName(CONNJ3, 19, 19)
+ChangePinName(CONNJ3, 18, 18)
+ChangePinName(CONNJ3, 17, 17)
+ChangePinName(CONNJ3, 16, 16)
+ChangePinName(CONNJ3, 15, 15)
+ChangePinName(CONNJ3, 14, 14)
+ChangePinName(CONNJ3, 13, 13)
+ChangePinName(CONNJ3, 12, 12)
+ChangePinName(CONNJ3, 11, 11)
+ChangePinName(CONNJ3, 10, 10)
+ChangePinName(CONNJ3, 9, 9)
+ChangePinName(CONNJ3, 8, 8)
+ChangePinName(CONNJ3, 7, 7)
+ChangePinName(CONNJ3, 6, 6)
+ChangePinName(CONNJ3, 5, 5)
+ChangePinName(CONNJ3, 4, 4)
+ChangePinName(CONNJ3, 3, 3)
+ChangePinName(CONNJ3, 2, 2)
+ChangePinName(CONNJ3, 1, 1)
